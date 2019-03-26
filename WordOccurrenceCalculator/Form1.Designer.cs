dd5c9b2f8159856160af571cbf002a9a2b41b5c6
Zac Long <zzl0100@tigermail.auburn.edu>
2019-03-25 20:37:18 -0400

    Changed textbox propertoes to be multiline.

--- WordOccurrenceCalculator/Form1.Designer.cs
+++ WordOccurrenceCalculator/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -37,20 +36,14 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(45, 36);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(177, 111);
             this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(45, 54);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(45, 113);
+            this.button1.Location = new System.Drawing.Point(45, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -60,7 +53,7 @@
             // resultLabel
             // 
             this.resultLabel.AutoSize = true;
-            this.resultLabel.Location = new System.Drawing.Point(197, 43);
+            this.resultLabel.Location = new System.Drawing.Point(185, 163);
             this.resultLabel.Name = "resultLabel";
             this.resultLabel.Size = new System.Drawing.Size(37, 13);
             this.resultLabel.TabIndex = 3;
@@ -73,7 +66,6 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -85,7 +77,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label resultLabel;
     }
