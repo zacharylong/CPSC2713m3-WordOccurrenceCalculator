e173b75355f5c12afdb38dd06c0c745c9b3241b5
Zac Long <zzl0100@tigermail.auburn.edu>
2019-03-31 13:52:18 -0400

    Fixed some references for nunit testing

--- WordOccurrencesTest/Class1.cs
+++ WordOccurrencesTest/Class1.cs
@@ -1,11 +1,14 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordOccurrenceCalculator;
+using WordOccurrenceCalculator;
 
-namespace WordOccurrencesTest
+namespace WordOccurrenceCalculator
 {
     //decorate with video example
     [TestFixture]
@@ -17,10 +20,12 @@ namespace WordOccurrencesTest
         {
             //Arrange
             //Setup required to run test
-
+            String stringTest = "one two three test two three";
+            //stringAsArrayList.Add("test one two three one");
 
             //Act
             //Code you want to test
+            var actualOutput = WordOccurrence.calculateOccurrences(stringTest);
 
             //Assert
             //Acts as expected?
